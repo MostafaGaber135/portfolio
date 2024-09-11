b9a48af14b8b67c564c9c241a93a3a9822b46409
MostafaGaber135 <mostafa20-00540@student.eelu.edu.eg>
2024-09-12 00:29:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Validate the form inputs here (if needed)
+
+    alert('Thank you for your message. I will get back to you soon!');
+});
